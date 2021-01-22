43c1403e3269a545f1127b84e58b7bbcf811a9bb
=kanishq1 <=kanishqkhandelwal@gmail.com>
2021-01-22 12:31:49 +0530

    initial commit

---
+++ client/src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<>
+			<div className="header" style={{ background: "#C6DDFF" }}>
+				<h1 className="m-0 font-samarkan font-100">
+					<span style={{ color: "#0056D1" }}>Vaibhav</span> <span style={{ color: "#019000" }}>Hospital</span>
+				</h1>
+				<h3 className="sub-heading">
+					<span style={{ color: "#0056D1" }}>Vada</span> acchi{" "}
+					<span style={{ color: "#019000" }}>Health</span> ka{" "}
+				</h3>
+			</div>
+		</>
+	);
+}
